9b558fb3c07845cc55018c24082cadad03a77ccf
KyawThiha111 <143880386+KyawThiha111@users.noreply.github.com>
2025-02-16 06:54:11 -0800

    Background Changer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css';
-import Counter from './Component/Counter';
+import ColorChanger from './Component/ColorChanger';
 function App() {
-  const [number,setNumber] = useState(0)
- 
     return (
       <div>
-       <Counter number={number} setNumber={setNumber}/>
+       <ColorChanger/>
       </div>
     )
 }
---
+++ src/Component/ColorChanger.jsx
@@ -0,0 +1,31 @@
+
+import { useState } from "react";
+const colors = ["bg-sky-500","bg-pink-500","bg-black"]
+function ColorChanger(){
+ const [backgroundColor, setBackGroundColor] = useState("bg-black");
+ 
+ const ChangeBackGround = (color)=>{
+    setBackGroundColor(color)
+ }
+ return(
+    <section>
+        <div className={`${backgroundColor} w-full h-[100vh] flex justify-center items-center`}>
+            <div className="w-[80%] md:w-[40%] h-[200px] bg-amber-800 text-white flex flex-col justify-center items-center gap-5">
+               <div className="text-center">
+               <p>Change background Color!</p>
+               <p>Click on the buttons!</p>
+               </div>
+               <div className="flex justify-evenly w-[50%]">
+                {colors.map((color,index)=>{
+                    return (
+                      <div key={index} onClick={()=>setBackGroundColor(color)} className={`${color} w-5 h-5 rounded-4xl cursor-pointer hover:border border-white`}></div>
+                    )
+                })}
+               </div>
+            </div>
+        </div>
+    </section>
+ )
+}
+
+ export default ColorChanger; 
