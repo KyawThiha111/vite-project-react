9d5e44ab445d1d40eb7dfd866e0dd86acf9d7fa3
KyawThiha111 <143880386+KyawThiha111@users.noreply.github.com>
2025-03-04 21:41:41 -0800

    UseEffect Recap with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css';
 import Counter from './Component/Counter';
 function App() {
-  const [number,setNumber] = useState(0)
- 
+  const [number,setNumber] = useState(0);
+  const [bgColor,setBgColor] = useState("bg-blue-200");
+  const [countSize,setCountSize] = useState("text-2xl")
+     useEffect(()=>{
+       bgColor==="bg-blue-200"?setBgColor("bg-pink-400"):setBgColor("bg-blue-200");
+       setCountSize("text-6xl");
+       const timeOutid = setTimeout(()=>{
+        setCountSize("text-2xl")
+       },1000)
+       return ()=> clearTimeout(timeOutid)
+     },[number])
     return (
       <div>
-       <Counter number={number} setNumber={setNumber}/>
+       <Counter number={number} bgColor={bgColor} countSize={countSize} setNumber={setNumber}/>
       </div>
     )
 }
--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
 function Counter(props){
- const {number,setNumber} = props;
+ const {number,bgColor,countSize,setNumber} = props;
 
  const Increasement =()=>{
     setNumber(number+1);
@@ -12,12 +12,14 @@ function Counter(props){
     console.log(number)
  }
     return(
-        <div className='counter-wrapper'>
-            <p className="numbertext">Number:<span className="number">{number}</span></p>
-           <div className="button_container">
-           <button className="button" onClick={()=>Increasement()}>Increase</button>
-           <button className="button" onClick={()=>Decreasement()}>Decrease</button>  
+        <div className={`w-full bg-black h-screen flex items-center justify-center`}>
+         <div className={`${bgColor} py-10 flex flex-col items-center gap-6 justify-center sm:w-3/4 md:w-1/4`}>
+         <p className="text-xl font-bold">Number:<span className={`${countSize} ml-3`}>{number}</span></p>
+           <div className="flex justify-between gap-6">
+           <button className="bg-white rounded-4xl w-25 py-2 hover:w-30 hover:py-3 transition-all duration-100" onClick={()=>Increasement()}>Increase</button>
+           <button className="bg-white rounded-4xl w-25 py-2 hover:w-30 hover:py-3 transition-all duration-100" onClick={()=>Decreasement()}>Decrease</button>  
            </div>
+         </div>
         </div>
     )
 }
