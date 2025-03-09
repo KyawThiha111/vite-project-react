4e6149ebf12b138bf3a946db9aba9e718a7c5212
KyawThiha111 <143880386+KyawThiha111@users.noreply.github.com>
2025-03-09 07:07:58 -0700

    Asan

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,27 @@ import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css';
-import Counter from './Component/Counter';
+import React from 'react';
+import { createContext } from 'react';
+import  {Developer} from './Component/Developer';
+
+export const myContext = createContext(undefined);
 function App() {
-  const [number,setNumber] = useState(0);
-  const [bgColor,setBgColor] = useState("bg-blue-200");
-  const [countSize,setCountSize] = useState("text-2xl")
-     useEffect(()=>{
-       bgColor==="bg-blue-200"?setBgColor("bg-pink-400"):setBgColor("bg-blue-200");
-       setCountSize("text-6xl");
-       const timeOutid = setTimeout(()=>{
-        setCountSize("text-2xl")
-       },1000)
-       return ()=> clearTimeout(timeOutid)
-     },[number])
-    return (
-      <div>
-       <Counter number={number} bgColor={bgColor} countSize={countSize} setNumber={setNumber}/>
-      </div>
-    )
+   const [developer,setDeveloper] = useState({
+    name:"Kyaw Thiha",
+    age:21,
+    skills:["Mern Stack","Python","mySQL"]
+   })
+   const handleSetDeveloperskills = (newData)=>{
+     setDeveloper({...developer,skills:[...developer.skills,newData]})
+   }
+   return(
+     <div>
+       <myContext.Provider value={{developer,handleSetDeveloperskills}}>
+       <Developer/>
+       </myContext.Provider>
+     </div>
+   )
 }
 
 export default App;
